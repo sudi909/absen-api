c0a9cb6d10c0d2375cf4b8365f5d5eb87e05a035
stprasst <stefanus.eko.p@gmail.com>
2020-09-01 15:51:54 +0700

    update header & Index

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,3 +1,21 @@
 <head>
 
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Yayasan Vitka Contact Tracing">
+    <meta name="author" content="IT Yayasan Vitka">
+    <title>YVCT | Yayasan Vitka Contact Tracing</title>
+
+    <!-- Favicons-->
+    <link rel="shortcut icon" href="img/logoyv.png" type="image/x-icon">
+
+    <!-- GOOGLE WEB FONT -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,500,600,700&display=swap" rel="stylesheet">
+
+    <!-- BASE CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/menu.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+	<link href="css/vendors.css" rel="stylesheet">
 </head>
