2a64e4bc83ee1700cf9a97028821dd1e1e74657e
Adli I. Ifkar <adly.shadowbane@gmail.com>
2020-09-02 14:46:03 +0700

    Autofix

--- app/Http/Controllers/Api/V1/VisitorAttendanceApiController.php
+++ app/Http/Controllers/Api/V1/VisitorAttendanceApiController.php
@@ -8,22 +8,35 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
+/**
+ * Class VisitorAttendanceApiController
+ *
+ * @package App\Http\Controllers\Api\V1
+ */
 class VisitorAttendanceApiController extends Controller
 {
+    /**
+     * @param  Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function tapIn(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|min:3|max:100',
-            'location' => 'required',
-            'phone' => 'required|digits_between:6,15',
-            'address' => 'nullable|string',
-            'keperluan' => 'nullable|string',
-        ], [
-            'name.required' => 'Oops, Nama harus diisi',
-            'name.min' => 'Oops, Nama minimal 3 karakter',
-            'location.required' => 'Oops, Lokasi anda tidak diketahui',
-            'phone.required' => 'Oops, Nomor Telepon harus diisi',
-        ]);
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'name' => 'required|min:3|max:100',
+                'location' => 'required',
+                'phone' => 'required|digits_between:6,15',
+                'address' => 'nullable|string',
+                'keperluan' => 'nullable|string',
+            ],
+            [
+                'name.required' => 'Oops, Nama harus diisi',
+                'name.min' => 'Oops, Nama minimal 3 karakter',
+                'location.required' => 'Oops, Lokasi anda tidak diketahui',
+                'phone.required' => 'Oops, Nomor Telepon harus diisi',
+            ]
+        );
 
         if ($validator->fails()) {
             return response()->json(['errors' => true, 'message' => $validator->errors()->first()], 401);
@@ -32,13 +45,16 @@ class VisitorAttendanceApiController extends Controller
         try {
             Visitor::create($request->all());
         } catch (\Exception $exception) {
-            return response()->json([
-                'errors' => true,
-                'message' => $exception->getMessage(),
-            ], 500);
+            return response()->json(
+                [
+                    'errors' => true,
+                    'message' => $exception->getMessage(),
+                ],
+                500
+            );
         }
 
-        $message = "Selamat Datang, " . $request->name;
+        $message = "Selamat Datang, ".$request->name;
         return response()->json(['errors' => false, 'message' => $message]);
     }
 }
--- app/Http/Controllers/VisitorController.php
+++ app/Http/Controllers/VisitorController.php
@@ -3,9 +3,18 @@
 
 namespace App\Http\Controllers;
 
-
+/**
+ * Class VisitorController
+ *
+ * @package App\Http\Controllers
+ */
 class VisitorController extends Controller
 {
+    /**
+     * Show visitor page
+     *
+     * @return \Illuminate\View\View|\Laravel\Lumen\Application
+     */
     public function index()
     {
         return view('visitor');
