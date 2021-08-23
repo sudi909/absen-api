c890f231f3964852a028c05348bd754c31c76e4e
Sudiharyanto Lika <sudiharyantolika@gmail.com>
2021-08-23 15:12:24 +0700

    Change table members to update table internal_ids Update IDS Model

--- app/Models/Identifier.php
+++ app/Models/Identifier.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Identifier extends Model
 {
     protected $table = 'internal_identifiers';
-    protected $fillable = ['identifier', 'name', 'unit'];
+    protected $fillable = ['identifier', 'name', 'unit', 'vaccine_count'];
     protected $dates = [
         'created_at',
         'updated_at',
--- database/migrations/2021_08_23_150628_add_column_vaccinecount_on_internal_ids_table.php
+++ database/migrations/2021_08_23_150628_add_column_vaccinecount_on_internal_ids_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMembersTable extends Migration
+class AddColumnVaccineCountsOnInternalIdentifiersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,8 @@ class CreateMembersTable extends Migration
      */
     public function up()
     {
-        Schema::create('members', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->uuid('uuid');
-            $table->string('name');
-            $table->char('nim', 15);
+        Schema::table('internal_identifiers', function (Blueprint $table) {
             $table->integer('vaccine_count');
-            $table->timestamps();
         });
     }
 
@@ -30,6 +25,8 @@ class CreateMembersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('members');
+        Schema::table('internal_identifiers', function (Blueprint $table) {
+            $table->dropColumn('vaccine_count');
+        });
     }
 }
