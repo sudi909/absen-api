b918c22aeff4d4985b6aec237a4aaac59523cd11
Sudiharyanto Lika <sudiharyantolika@gmail.com>
2021-08-23 14:51:59 +0700

    Add new tqable migration members

---
+++ database/migrations/2021_08_23_111558_create_members_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->uuid('uuid');
+            $table->string('name');
+            $table->char('nim', 15);
+            $table->integer('vaccine_count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
